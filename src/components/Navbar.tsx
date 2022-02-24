635fb45b6413210a812c5975441ccc54e4d05fde
saralindqvist <sarathereselindqvist@hotmail.com>
2022-02-24 18:40:08 +0100

    Added navbar and searchform

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,23 @@
+import './Navbar.css';
+
 function Navbar() {
+  
     return (
-        <div>This will be a navbar soon...</div>
+        <section className='section search'>
+        <form className='search-form'>
+          <div className='form'>
+            <label htmlFor='name'>What do you feel like today?</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+            />
+          <div className='filter-name'><p>Espresso | Drip brew | French press</p></div>
+          </div>
+        </form>
+        
+      </section>
+     
     )
 }
 
