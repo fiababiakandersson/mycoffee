7cf90d364433c23ee73fda6df9d5257912440d02
Millie Cheung <cheungmillie9@gmail.com>
2022-02-25 13:22:33 +0100

    Got in an extra icon of heart instead of using class name

--- src/components/HeartIcon.tsx
+++ src/components/HeartIcon.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 import { CSSProperties } from "react";
 
 interface Props {
@@ -12,8 +13,8 @@ function HeartIcon(props: Props) {
     <FontAwesomeIcon
       onClick={handleLike}
       //onClick={props.isLiked ? true : false}
-      icon={faHeart}
-      style={props.isLiked ? heartLikedStyle : heartStyle}
+      icon={props.isLiked ? faHeartRegular : faHeartSolid}
+      style={heartStyle}
     />
   );
 }
@@ -23,12 +24,6 @@ const handleLike = () => {
 };
 
 const heartStyle: CSSProperties = {
-  fontSize: "2rem",
-  marginLeft: "1rem",
-  color: "green",
-};
-
-const heartLikedStyle: CSSProperties = {
   fontSize: "2rem",
   marginLeft: "1rem",
   color: "red",
