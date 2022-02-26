3bc2216c16c51900450a45343017ec2fab7c1f02
Millie Cheung <cheungmillie9@gmail.com>
2022-02-27 00:09:28 +0100

    added images from data to jsonData

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,43 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CoffeeList from "./CoffeeList";
 import Contact from "./Contact";
 import Layout from "./Layout";
+import { coffeeData } from "../data";
+
+export interface JSONValue {
+  title: string;
+  id: number;
+  description: string;
+  ingredients: string[];
+  image: string;
+}
+
+// export interface JSONValues extends Array<JSONValue> {}
+// let data2: JSONValues;
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("https://api.sampleapis.com/coffee/hot");
+      const jsonData: [] = await response.json();
+      return compareData(jsonData);
+    };
+    getData();
+  }, []);
 
-   const [coffeeData, setCoffeeData] = useState([]);
-   useEffect(() => {
-     const getData = async () => {
-       const response = await fetch("https://api.sampleapis.com/coffee/hot");
-       const jsonData = await response.json();
-       console.log(jsonData);
-     };
-     getData();
-   }, []);
+  const compareData = (jsonData: JSONValue[]) => {
+    for (let i = 0; i < coffeeData.length; i++) {
+      for (let x = 0; x < jsonData.length; x++) {
+        if (jsonData[x].title === coffeeData[i].name) {
+          jsonData[x].image = coffeeData[i].image;
+        }
+      }
+    }
+    return jsonData;
+    // console.log(jsonData);
+  };
 
   return (
     <BrowserRouter>
