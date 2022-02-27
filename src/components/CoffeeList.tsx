64b114788faa1062638f9764193602fde34506a5
Millie Cheung <cheungmillie9@gmail.com>
2022-02-27 19:44:09 +0100

    added function to save likes to local storage

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 interface State {
   coffees: JSONValues;
+  likedCoffeeArr: number[];
 }
 
 class CoffeeList extends Component<Props, State> {
@@ -18,14 +19,35 @@ class CoffeeList extends Component<Props, State> {
     super(props);
     this.state = {
       coffees: this.props.newData,
+      likedCoffeeArr: JSON.parse(localStorage.likedCoffee || "[]"),
     };
   }
+
+  updateLS = (arr: number[]) => {
+    localStorage.likedCoffee = JSON.stringify(arr);
+  };
+
+  updateLike = (id: number) => {
+    const arr = this.state.likedCoffeeArr;
+    const coffeeIndex = arr.indexOf(id);
+    if (coffeeIndex !== -1) {
+      // as long as not -1, it means the item exists in the array
+      arr.splice(coffeeIndex, 1); // remove
+    } else {
+      arr.push(id); // add
+    }
+    this.updateLS(arr);
+  };
 
   render() {
     return (
       <div style={rootStyle}>
         {this.state.coffees.map((coffee) => (
-          <CoffeeCard key={coffee.id} data={coffee} />
+          <CoffeeCard
+            key={coffee.id}
+            data={coffee}
+            onLikeChange={() => this.updateLike(coffee.id)}
+          />
         ))}
       </div>
     );
