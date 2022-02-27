36b9908bb28f59d756ac83cc8723f91bab1aba76
Millie Cheung <cheungmillie9@gmail.com>
2022-02-27 17:10:12 +0100

    added ErrorBoundary with fallBackComponent

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,20 @@
 import { CSSProperties } from "react";
+import { useNavigate } from "react-router-dom";
 import errorImg from "../assets/error-coffee.webp";
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div style={rootStyle}>
       <h1>Oops!</h1>
       <img style={imgStyle} src={errorImg} alt="spilled coffee" />
       <h2>This page doesn't seem to exist.</h2>
+      <button onClick={handleClick}>back to Home</button>
     </div>
   );
 }
