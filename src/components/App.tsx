dc65094d62a251ff845174b10e02cd05a58d3e9f
Millie Cheung <cheungmillie9@gmail.com>
2022-02-27 11:49:17 +0100

    applied the combined data to app, coffee card and coffee list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,17 +14,23 @@ export interface JSONValue {
   image: string;
 }
 
-// export interface JSONValues extends Array<JSONValue> {}
-// let data2: JSONValues;
+export interface JSONValues extends Array<JSONValue> {}
 
 function App() {
+  const [newData, setData] = useState<JSONValues>([]);
+
   useEffect(() => {
-    const getData = async () => {
-      const response = await fetch("https://api.sampleapis.com/coffee/hot");
-      const jsonData: [] = await response.json();
-      return compareData(jsonData);
-    };
-    getData();
+    fetch("https://api.sampleapis.com/coffee/hot")
+      // get response object with json method on that
+      .then((response) => {
+        return response.json();
+      })
+      // tag on another then method whereby we get the data
+      .then((jsonData: JSONValue[]) => {
+        jsonData = compareData(jsonData);
+        setData(jsonData);
+        console.log(jsonData);
+      });
   }, []);
 
   const compareData = (jsonData: JSONValue[]) => {
@@ -32,18 +38,24 @@ function App() {
       for (let x = 0; x < jsonData.length; x++) {
         if (jsonData[x].title === coffeeData[i].name) {
           jsonData[x].image = coffeeData[i].image;
+        } else {
+          // TO BE FOLLOWED-UP: insert the src of a generic image to below
+          jsonData[x].image =
+            "FOLLOW UP!!!: a generic image for those without a pic";
         }
       }
     }
     return jsonData;
-    // console.log(jsonData);
   };
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<CoffeeList />} />
+          {/* logic: if the left part is an empty array (which returns fault) then it won't load the right; and if left is true, it goes to right and output the right on the screen */}
+          {newData.length > 0 && (
+            <Route index element={<CoffeeList newData={newData} />} />
+          )}
           <Route path="saved" element={<div>Saved</div>} />
           <Route path="about" element={<div>About</div>} />
           <Route path="contact" element={<Contact />} />
