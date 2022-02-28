286827133d07e886d723de2dfd7429ac663dbf53
Millie Cheung <cheungmillie9@gmail.com>
2022-02-28 16:48:18 +0100

    inserted coffee cards from the liked array

--- src/components/SavedCoffeeList.tsx
+++ src/components/SavedCoffeeList.tsx
@@ -1,21 +1,42 @@
-import { CSSProperties, useState } from "react";
+import { CSSProperties } from "react";
 import { Coffee } from "./App";
 import CoffeeCard from "./CoffeeCard";
 interface Props {
-  data: Coffee[];
+  coffees: Coffee[];
 }
+
 function SavedCoffeeList(props: Props) {
-  const likedCoffeeArr = localStorage.getItem("likedCoffee");
-  console.log(likedCoffeeArr);
+  const thisfunction = (id: string) => {
+    console.log("hello");
+  };
+
+  let savedList: Coffee[] = [];
+  let list = localStorage.getItem("likedCoffee");
+  if (list) {
+    let parsedList: string[] = JSON.parse(list);
+    for (let i = 0; i < props.coffees.length; i++) {
+      for (let x = 0; x < parsedList.length; x++) {
+        if (parsedList[x] === props.coffees[i].id) {
+          savedList.push(props.coffees[i]);
+        }
+        console.log(savedList);
+      }
+    }
+  }
 
   return (
     <div style={rootStyle}>
-      {props.data.map((coffee) => (
-        <p>{coffee.id}</p>
+      {savedList.map((item) => (
+        <CoffeeCard
+          key={item.id}
+          data={item}
+          onLikeChange={() => thisfunction(item.id)}
+        />
       ))}
     </div>
   );
 }
+
 const rootStyle: CSSProperties = {
   display: "flex",
   flexDirection: "row",
