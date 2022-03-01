6d91c615fab8a317aeefb1f3d5f0d8fb3f93964c
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-03-01 11:52:09 +0100

    Made links responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,29 +4,30 @@ import './Header.css';
 
 function Header() {
     return (
-    <nav className='header'> 
-        <div className='logo-div'>
-            <Link to='/'>
-            <img src={Logo} alt='mycoffee logo' className='logo'/>
-            </Link>
+      <nav className="header">
+        <div className="logo-div">
+          <Link to="/">
+            <img src={Logo} alt="mycoffee logo" className="logo" />
+          </Link>
         </div>
-
-            <ul className='nav-links'>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/saved'>Saved</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-    </nav>
-  );
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/saved">Saved</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link className="responsive-link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
 }
 
 export default Header;
