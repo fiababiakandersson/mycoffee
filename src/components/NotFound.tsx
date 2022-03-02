7f8ed5dda4d7c8d7d89b7fa50d459d918a2616bd
Millie Cheung <cheungmillie9@gmail.com>
2022-03-02 10:29:04 +0100

    replaced the button element with the backButton component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,20 +1,14 @@
 import { CSSProperties } from "react";
-import { useNavigate } from "react-router-dom";
 import errorImg from "../assets/error-coffee.webp";
+import BackButton from "./shared/BackButton";
 
 function NotFound() {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(-1);
-  };
-
   return (
     <div style={rootStyle}>
       <h1>Oops!</h1>
       <img style={imgStyle} src={errorImg} alt="spilled coffee" />
       <h2>This page doesn't seem to exist.</h2>
-      <button onClick={handleClick}>back to Home</button>
+      <BackButton content="Back" />{" "}
     </div>
   );
 }
@@ -23,6 +17,7 @@ const rootStyle: CSSProperties = {
   minHeight: "25rem",
   textAlign: "center",
   color: "#642801",
+  paddingBottom: "6rem",
 };
 
 const imgStyle: CSSProperties = {
